646e24ac31fa381befbe323d315c415fffa77b37
Nickolay Erohin <n.erohin@inpas.ru>
2020-02-19 15:23:46 +0300

    Adjust getting configuration remotely and gateway server routes

--- one-client/src/main/java/spring/boot/cloud/test/OneClient.java
+++ one-client/src/main/java/spring/boot/cloud/test/OneClient.java
@@ -1,27 +1,39 @@
 package spring.boot.cloud.test;
 
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
 @RequestMapping("/one-client")
+@Configuration
 public class OneClient {
 
-//    @Value("${user.role}")
-//    private String role;
+    @Value("${user.role}")
+    private String role;
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         SpringApplication.run(OneClient.class, args);
     }
 
